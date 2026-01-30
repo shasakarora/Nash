61af72d4addd4e25cb9afce942e78d2e58352685
Keshav Bansal <130533959+kingspin-42@users.noreply.github.com>
2026-01-30 18:45:19 +0530

    initial commit

---
+++ server/src/config/db.ts
@@ -0,0 +1,18 @@
+import {Pool} from "pg";
+import dotenv from "dotenv";
+
+dotenv.config()
+
+const pool = new Pool({
+  user: process.env.USER,
+  password: process.env.PASSWORD,
+  database: process.env.DATABASE,
+  host: process.env.HOST,
+  port: parseInt(process.env.DBPORT),
+});
+
+pool.on("error", (err: any) => {
+  console.error("[db] unexpected error on client", err);
+});
+
+export default pool;
