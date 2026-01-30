d51164d044b80e635829421707173f2121edf309
Vedansh-Sethi <vedansh25sethi@gmail.com>
2026-01-31 03:27:09 +0530

    [UI] : Created Group Card

---
+++ app/lib/pages/home/widgets/group_card.dart
@@ -0,0 +1,61 @@
+import 'package:app/config/theme.dart';
+import 'package:flutter/material.dart';
+
+class GroupCard extends StatelessWidget {
+  final String groupName;
+  final int memberNumber;
+
+  const GroupCard({
+    super.key,
+    required this.groupName,
+    required this.memberNumber,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: MediaQuery.of(context).size.width,
+      margin: const EdgeInsets.only(right: 24.0),
+      padding: const EdgeInsets.all(16.0),
+      decoration: BoxDecoration(
+        color: context.colorScheme.surface,
+        borderRadius: BorderRadius.circular(16.0),
+      ),
+      child: Row(
+        children: [
+          CircleAvatar(
+            radius: 28,
+            backgroundImage: AssetImage('assets/pfp.png'),
+          ),
+          SizedBox(width: 12),
+          Expanded(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  groupName,
+                  style: TextStyle(
+                    fontSize: 14,
+                    fontWeight: FontWeight.bold,
+                    color: context.colorScheme.onSurface,
+                  ),
+                  maxLines: 1,
+                  overflow: TextOverflow.ellipsis,
+                ),
+                SizedBox(height: 4),
+                Text(
+                  '$memberNumber members',
+                  style: TextStyle(
+                    color: context.colorScheme.onSurfaceVariant,
+                    fontSize: 12
+                  )
+                )
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
