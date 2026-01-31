e97a8706ee705dd6f5fd5d672b6338887056d0ec
Vedansh-Sethi <vedansh25sethi@gmail.com>
2026-01-31 20:56:29 +0530

    [UI] : Created Group Bet Details Page

--- app/lib/pages/group_bets/widgets/group_bet_details.dart
+++ app/lib/pages/group_bets/widgets/group_bet_details.dart
@@ -1,25 +1,23 @@
+import 'package:app/config/theme.dart';
+import 'package:app/extensions/number.dart';
 import 'package:flutter/material.dart';
 
-import '/config/theme.dart';
-import '/extensions/number.dart';
-
-class BetDetailsCard extends StatelessWidget {
-  const BetDetailsCard({super.key, required this.data});
-
+class GroupBetDetailsCard extends StatelessWidget {
   final Map<String, dynamic> data;
+  const GroupBetDetailsCard({super.key, required this.data});
 
   @override
   Widget build(BuildContext context) {
     return Card(
       child: Padding(
-        padding: EdgeInsets.symmetric(horizontal: 32, vertical: 24),
+        padding: EdgeInsets.symmetric(horizontal: 24, vertical: 16),
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
             Text(
               "\$${(data["total_pot"] as num).formatWithCommas()}",
               style: TextStyle(
-                fontSize: 52,
+                fontSize: 30,
                 color: context.colorScheme.secondary,
                 fontWeight: FontWeight.bold,
               ),
@@ -28,22 +26,22 @@ class BetDetailsCard extends StatelessWidget {
             Text(
               "EXPECTED PAYOUT: \$${data['my_bet']['expected_payout']}",
               style: TextStyle(
-                fontSize: 18,
+                fontSize: 14,
                 color: context.colorScheme.onSurfaceVariant,
               ),
             ),
             Text(
               "BET PLACED: \$${data['my_bet']['amount']}",
               style: TextStyle(
-                fontSize: 18,
+                fontSize: 14,
                 color: context.colorScheme.onSurfaceVariant,
               ),
             ),
-            const SizedBox(height: 32),
+            const SizedBox(height: 16),
             Text(
               data['title'].toString(),
               style: TextStyle(
-                fontSize: 22,
+                fontSize: 20,
                 fontWeight: FontWeight.bold,
                 color: context.colorScheme.onSurface,
               ),
