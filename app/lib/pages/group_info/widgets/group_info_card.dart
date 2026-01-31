02151a85b9e6247f8ac7aca9796e915c2cb0d74f
Vedansh-Sethi <vedansh25sethi@gmail.com>
2026-01-31 16:35:01 +0530

    [UI] : Created Group Info Card

---
+++ app/lib/pages/group_info/widgets/group_info_card.dart
@@ -0,0 +1,50 @@
+import 'package:app/config/theme.dart';
+import 'package:flutter/material.dart';
+
+class GroupInfoCard extends StatelessWidget {
+  const GroupInfoCard({super.key, required this.data});
+
+  final Map<String, dynamic> data;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Container(
+        width: MediaQuery.of(context).size.width,
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(25.0),
+          color: context.colorScheme.surface,
+        ),
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            children: [
+              CircleAvatar(
+                backgroundImage: AssetImage('assets/pfp.png'),
+                radius: 100,
+              ),
+              SizedBox(height: 16.0),
+              Text(
+                data['title'].toString(),
+                style: TextStyle(
+                  color: context.colorScheme.onSurface,
+                  fontSize: 16,
+                ),
+              ),
+              SizedBox(height: 12),
+              Text(
+                data['description'].toString(),
+                style: TextStyle(
+                  color: context.colorScheme.onSurfaceVariant,
+                  fontSize: 12,
+                ),
+                textAlign: TextAlign.center,
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
